cc790a3ec74064cb84a367124488261b4fa891e3
“Milan <milan.stankovic@ctd.ai>
2023-08-03 15:50:36 +0200

    .

---
+++ MaintenancePage.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { computed } from "vue";
+import { useRouter } from "vue-router";
+
+const iFrameSource = computed(
+  () => "https://page.ctdmaintenancepage.workers.dev/"
+); // mozda ovo da bude promenljiva?
+
+const router = useRouter();
+
+if (maintenanceFlag.value) {
+  router.push({ name: "maintenance" });
+}
+</script>
+
+<template>
+  <div class="h-[calc(100vh-72px)] overflow-hidden">
+    <iframe class="h-full w-full" :src="iFrameSource"></iframe>
+  </div>
+</template>
